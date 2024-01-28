51447999a6d8afd1ee8f2738fec131f9f9b96284
Neo319 <alexmnevins@gmail.com>
2024-01-28 16:42:24 -0500

    add images and logo using populate-content.js

--- src/populate-content.js
+++ src/populate-content.js
@@ -13,7 +13,34 @@ const populate = function () {
     temp.textContent = "content...";
     content.appendChild(temp);
 
+    //header logo    
+    const logo = new Image()
+    logo.src = "../src/logo.png"
+    logo.id = "logo"
+    header.insertBefore(logo, header.firstChild)
 
+
+
+    //first image
+    const firstCard = document.createElement("div");
+    const firstCardImage = new Image();
+    firstCardImage.src = '../src/food-1.jpg';
+    firstCardImage.id = 'food-1';
+    firstCardImage.alt = "french toast with berries";
+
+    firstCard.appendChild(firstCardImage);
+    content.appendChild(firstCard);
+
+
+    //second image
+    const secondCard = document.createElement("div");
+    const secondCardImage = new Image();
+    secondCardImage.src = '../src/food-2.jpg';
+    secondCardImage.id = 'food-2';
+    secondCardImage.alt= "chicken noodle soup";
+
+    secondCard.appendChild(secondCardImage);
+    content.appendChild(secondCard);
 }
 
 export default populate;
